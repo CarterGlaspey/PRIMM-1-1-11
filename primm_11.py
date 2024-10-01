bd719708bb409b47d898adff147df3640fc1402e
CarterGlaspey <cglaspey9@gmail.com>
2024-10-01 21:55:22 +0000

    Update code and questions

--- primm_11.py
+++ primm_11.py
@@ -5,9 +5,9 @@ Name - Date
 """
 
 def main():
-  
-    num1: int = input("Enter a number: ")
-    num2: int = input("Enter another number: ")
+  #allows the user to enter a number and then another number which the program adds
+    num1: int = int(input("Enter a number: "))
+    num2: int = int(input("Enter another number: "))
     total: int = num1+num2
 
     print(f"{num1} + {num2} = {total}")
